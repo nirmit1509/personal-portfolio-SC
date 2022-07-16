3b2aa15902ae6efbd0eceb9e2a01ce44dac2a0b4
nirmit1509 <nirmitsabharwal15@gmail.com>
2022-07-16 21:52:36 +0530

    ArtWork ERC721

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -8,113 +8,125 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/security/ReentrancyGuard.sol';
 
 contract Airdrop is Ownable, ReentrancyGuard {
+  /**
+   * @dev Constructor for Airdrop contract
+   * @param _serviceFee service fee of network
+   */
+  constructor(uint256 _serviceFee) {
+    serviceFee = _serviceFee;
+  }
 
-    /**
-     * @dev Constructor for Airdrop contract
-     * @param _serviceFee service fee of network
-     */
-    constructor(uint256 _serviceFee) {
-        serviceFee = _serviceFee;
+  using SafeMath for uint256;
+
+  uint256 serviceFee;
+
+  /**
+   * @dev Emitted when `value` tokens are received by smart contract
+   * Note that `value` may be zero.
+   */
+  event Received(address sender, uint256 value);
+
+  /**
+   * @dev External function to set service fee of network
+   * Reverts if the caller is not owner
+   * @param _serviceFee The updated service fee for the contract
+   */
+  function changeServiceFee(uint256 _serviceFee) external onlyOwner {
+    serviceFee = _serviceFee;
+  }
+
+  /**
+   * @dev External function to transfer tokens to recepient addresses in bulk
+   * Reverts if reentrant call is made to the function
+   * Reverts if the length of addresses and amounts is not equal
+   * Reverts if service fee is not provided
+   * Reverts if this smart contract is not approved to transfer tokens
+   * @param tokenAddress The address of ERC20 token to be airdropped
+   * @param addresses The list of Ethereum addresses
+   * @param amounts The amount of tokens to be transferred to respective address
+   */
+  function transferTokenBulk(
+    IERC20 tokenAddress,
+    address[] calldata addresses,
+    uint256[] calldata amounts
+  ) external payable nonReentrant {
+    require(
+      addresses.length == amounts.length,
+      'addresses and amounts must be the same length'
+    );
+    address from = msg.sender;
+    uint256 total = 0;
+    for (uint256 i = 0; i < amounts.length; i++) {
+      total += amounts[i];
     }
-
-    using SafeMath for uint256;
-
-    uint256 serviceFee;
-
-    /**
-     * @dev Emitted when `value` tokens are received by smart contract
-     * Note that `value` may be zero.
-     */
-    event Received(address sender, uint256 value);
-
-    /**
-     * @dev External function to set service fee of network
-     * Reverts if the caller is not owner
-     * @param _serviceFee The updated service fee for the contract
-     */
-    function changeServiceFee(uint256 _serviceFee) external onlyOwner {
-        serviceFee = _serviceFee;
+    require(msg.value >= serviceFee, 'Service Fee not provided');
+    require(
+      tokenAddress.allowance(from, address(this)) >= total,
+      'not enough allowance'
+    );
+    for (uint256 i = 0; i < addresses.length; i++) {
+      SafeERC20.safeTransferFrom(tokenAddress, from, addresses[i], amounts[i]);
     }
+  }
 
-    /**
-     * @dev External function to transfer tokens to recepient addresses in bulk
-     * Reverts if reentrant call is made to the function
-     * Reverts if the length of addresses and amounts is not equal
-     * Reverts if service fee is not provided
-     * Reverts if this smart contract is not approved to transfer tokens
-     * @param tokenAddress The address of ERC20 token to be airdropped
-     * @param addresses The list of Ethereum addresses
-     * @param amounts The amount of tokens to be transferred to respective address
-     */
-    function transferTokenBulk(IERC20 tokenAddress, address[] calldata addresses, uint256[] calldata amounts)
-    external payable nonReentrant {
-        require(addresses.length == amounts.length, "addresses and amounts must be the same length");
-        address from = msg.sender;
-        uint256 total = 0;
-        for(uint i = 0; i < amounts.length; i++) {
-            total += amounts[i];
-        }
-        require(msg.value >= serviceFee, "Service Fee not provided");
-        require(tokenAddress.allowance(from, address(this)) >= total, "not enough allowance");
-        for (uint256 i = 0; i < addresses.length; i++) {
-            SafeERC20.safeTransferFrom(tokenAddress, from, addresses[i], amounts[i]);
-        }
+  /**
+   * @dev External function to transfer native currency to recepient addresses in bulk
+   * Reverts if reentrant call is made to the function
+   * Reverts if the length of addresses and amounts is not equal
+   * Reverts if enough currency is not paid (service fee + total transfer amount)
+   * @param addresses The list of Ethereum addresses
+   * @param amounts The amount of tokens to be transferred to respective address
+   */
+  function transferCurrencyBulk(
+    address[] calldata addresses,
+    uint256[] calldata amounts
+  ) external payable nonReentrant {
+    require(
+      addresses.length == amounts.length,
+      'addresses and amounts must be the same length'
+    );
+    uint256 total = 0;
+    for (uint256 i = 0; i < amounts.length; i++) {
+      total += amounts[i];
     }
-
-    /**
-     * @dev External function to transfer native currency to recepient addresses in bulk
-     * Reverts if reentrant call is made to the function
-     * Reverts if the length of addresses and amounts is not equal
-     * Reverts if enough currency is not paid (service fee + total transfer amount)
-     * @param addresses The list of Ethereum addresses
-     * @param amounts The amount of tokens to be transferred to respective address
-     */
-    function transferCurrencyBulk(address[] calldata addresses, uint256[] calldata amounts)
-    external payable nonReentrant {
-        require(addresses.length == amounts.length, "addresses and amounts must be the same length");
-        uint256 total = 0;
-        for(uint i = 0; i < amounts.length; i++) {
-            total += amounts[i];
-        }
-        require(msg.value >= total + serviceFee, "not enough value");
-        for (uint256 i = 0; i < addresses.length; i++) {
-            payable(addresses[i]).transfer(amounts[i]);
-        }
+    require(msg.value >= total + serviceFee, 'not enough value');
+    for (uint256 i = 0; i < addresses.length; i++) {
+      payable(addresses[i]).transfer(amounts[i]);
     }
+  }
 
+  /**
+   * @dev External function to transfer currency stuck in smart contract to EOA address
+   * Reverts if reentrant call is made to the function
+   * Reverts if the caller is not owner
+   * @param amount The amount of tokens to be transferred
+   */
+  function withdrawCurrency(uint256 amount) external onlyOwner nonReentrant {
+    payable(msg.sender).transfer(amount);
+  }
 
-    /**
-     * @dev External function to transfer currency stuck in smart contract to EOA address
-     * Reverts if reentrant call is made to the function
-     * Reverts if the caller is not owner
-     * @param amount The amount of tokens to be transferred
-     */
-    function withdrawCurrency(uint256 amount)
-    external onlyOwner nonReentrant {
-        payable(msg.sender).transfer(amount);
-    }
+  /**
+   * @dev External function to transfer tokens stuck in smart contract to EOA address
+   * Reverts if reentrant call is made to the function
+   * Reverts if the caller is not owner
+   * @param tokenAddress The address of ERC20 token to be recovered
+   * @param to The Ethereum addresses which will receive the tokens
+   * @param amount The amount of tokens to be transferred
+   */
+  function tokenRecovery(
+    IERC20 tokenAddress,
+    address to,
+    uint256 amount
+  ) external onlyOwner nonReentrant {
+    SafeERC20.safeTransfer(tokenAddress, to, amount);
+  }
 
-    
-    /**
-     * @dev External function to transfer tokens stuck in smart contract to EOA address
-     * Reverts if reentrant call is made to the function
-     * Reverts if the caller is not owner
-     * @param tokenAddress The address of ERC20 token to be recovered
-     * @param to The Ethereum addresses which will receive the tokens
-     * @param amount The amount of tokens to be transferred
-     */
-    function tokenRecovery(IERC20 tokenAddress, address to, uint256 amount)
-    external onlyOwner nonReentrant {
-        SafeERC20.safeTransfer(tokenAddress, to, amount);
-    }
-
-    /**
-     * The receive function is executed on a call to the contract with empty calldata. 
-     * This is the function that is executed on plain Ether transfers
-     * Event `Received` is emitted when plain ethers are transferred to this contract
-     */
-    receive() external payable {
-        emit Received(msg.sender, msg.value);
-    }
-
+  /**
+   * The receive function is executed on a call to the contract with empty calldata.
+   * This is the function that is executed on plain Ether transfers
+   * Event `Received` is emitted when plain ethers are transferred to this contract
+   */
+  receive() external payable {
+    emit Received(msg.sender, msg.value);
+  }
 }
---
+++ contracts/ArtWork.sol
@@ -0,0 +1,157 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
+
+contract ArtWork is ERC721URIStorage, Ownable {
+  using SafeMath for uint256;
+
+  struct Bid {
+    address bidder;
+    uint256 amount;
+  }
+
+  struct Art {
+    string category;
+    string media;
+    string size;
+  }
+
+  string private _name = 'ArtWork';
+  string private _symbol = 'ART';
+  uint256 private _totalSupply;
+  uint256 private _nextTokenId = 1000;
+  mapping(uint256 => uint256) private _biddingTime;
+  mapping(uint256 => Bid) private _maxBid;
+  mapping(uint256 => Art) private _tokenDetails;
+
+  event BidPlaced(address indexed bidder, uint256 tokenId, uint256 amount);
+
+  event Claimed(address indexed newOwner, uint256 tokenId, uint256 amount);
+
+  modifier biddingEnabled(uint256 tokenId) {
+    require(block.timestamp < _biddingTime[tokenId], 'bidding time is over!');
+    _;
+  }
+
+  constructor() ERC721(_name, _symbol) Ownable() {}
+
+  /// @dev Returns the total number of tokens (minted - burned) registered
+  function totalSupply() external view returns (uint256) {
+    return _totalSupply;
+  }
+
+  /// @dev Returns the token id of the next minted token
+  function nextTokenId() external view returns (uint256) {
+    return _nextTokenId;
+  }
+
+  /// @dev Mint a token. Only "owner" may call this function.
+  /// @param tokenURI The tokenURI of the the tokenURI
+  function mint(
+    string memory tokenURI,
+    uint256 startingBidValue,
+    uint256 bidTimeInSeconds,
+    string memory category,
+    string memory media,
+    string memory size
+  ) external onlyOwner {
+    _mint(owner(), _nextTokenId);
+    _setTokenURI(_nextTokenId, tokenURI);
+    Art memory art = Art(category, media, size);
+    _tokenDetails[_nextTokenId] = art;
+
+    Bid memory startBid = Bid(owner(), startingBidValue);
+    _maxBid[_nextTokenId] = startBid;
+    _biddingTime[_nextTokenId] = block.timestamp + bidTimeInSeconds;
+    _nextTokenId = _nextTokenId.add(1);
+    _totalSupply = _totalSupply.add(1);
+  }
+
+  function burn(address owner, uint256 tokenId) external onlyOwner {
+    require(ownerOf(tokenId) == owner, 'given address is not owner of Token');
+    _burn(tokenId);
+  }
+
+  function getCategory(uint256 tokenId) external view returns (string memory) {
+    require(_exists(tokenId), ' ArtWork: query for nonexistent token');
+    return _tokenDetails[tokenId].category;
+  }
+
+  function getMedia(uint256 tokenId) external view returns (string memory) {
+    require(_exists(tokenId), ' ArtWork: query for nonexistent token');
+    return _tokenDetails[tokenId].media;
+  }
+
+  function getSize(uint256 tokenId) external view returns (string memory) {
+    require(_exists(tokenId), ' ArtWork: query for nonexistent token');
+    return _tokenDetails[tokenId].size;
+  }
+
+  function highestBid(uint256 tokenId) external view returns (uint256) {
+    require(_exists(tokenId), ' ArtWork: query for nonexistent token');
+    return _maxBid[tokenId].amount;
+  }
+
+  function maxBidder(uint256 tokenId) external view returns (address) {
+    require(_exists(tokenId), ' ArtWork: query for nonexistent token');
+    return _maxBid[tokenId].bidder;
+  }
+
+  function biddingTimeRemaining(uint256 tokenId)
+    external
+    view
+    returns (uint256)
+  {
+    require(_exists(tokenId), ' ArtWork: query for nonexistent token');
+    if (block.timestamp < _biddingTime[tokenId]) {
+      return _biddingTime[tokenId] - block.timestamp;
+    } else {
+      return 0;
+    }
+  }
+
+  function bid(uint256 tokenId, uint256 amount)
+    external
+    biddingEnabled(tokenId)
+  {
+    Bid memory maxBid = _maxBid[tokenId];
+    uint256 maxAmount = maxBid.amount;
+
+    require(amount > maxAmount, 'Bidding price is less than highest bid');
+    maxBid.bidder = _msgSender();
+    maxBid.amount = amount;
+
+    _maxBid[tokenId] = maxBid;
+
+    emit BidPlaced(_msgSender(), tokenId, amount);
+  }
+
+  function claim(uint256 tokenId) external payable {
+    require(
+      _biddingTime[tokenId] < block.timestamp,
+      'bidding is still goining'
+    );
+    require(
+      _maxBid[tokenId].bidder == _msgSender(),
+      'caller is not the highest bidder'
+    );
+
+    uint256 amount = _maxBid[tokenId].amount;
+    require(amount <= msg.value, 'msg.Value is less than bid placed');
+
+    _transfer(owner(), _msgSender(), tokenId);
+
+    emit Claimed(_msgSender(), tokenId, amount);
+  }
+
+  function contractBalance() external view returns (uint256) {
+    return address(this).balance;
+  }
+
+  function redeemBalance() external onlyOwner {
+    payable(owner()).transfer(address(this).balance);
+  }
+}
